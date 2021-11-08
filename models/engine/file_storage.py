d5d2ad1422fc6efbefba3465f12ae51f754e05e5
luffy981 <jhonsmith981@hotmail.com>
2021-11-08 11:01:35 -0500

    File storage class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+class FileStorage:
+    """Class to serializes instances to a JSON file
+    and deserializes JSON file to instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
