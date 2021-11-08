257f721e4d5ba8238542023d33798f4b81707217
luffy981 <jhonsmith981@hotmail.com>
2021-11-08 11:41:41 -0500

    file storage fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,16 @@
+import json
+from models.base_model import BaseModel
+
+
+classes = {"BaseModel": BaseModel}
+
+
 class FileStorage:
     """Class to serializes instances to a JSON file
     and deserializes JSON file to instances"""
+    # string - path to the JSON file
     __file_path = "file.json"
+    #  dictionary - empty but will store all objects
     __objects = {}
 
     def all(self):
@@ -9,6 +18,27 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
+            # ex: to store a BaseModel object with id=12121212
+            # the key will be BaseModel.12121212
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        list_objects = {}
+        for key in self.__objects:
+            list_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w', encoding='UTF-8') as file:
+            json.dump(list_objects, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r', encoding='UTF-8') as file:
+                js = json.load(file)
+            for key in js:
+                self.__objects[key] = classes[js[key]["__class"]](**js[key])
+        except FileNotFoundError:
+            pass
