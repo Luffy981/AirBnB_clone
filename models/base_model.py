cabaf111fdbf9a25335e9b08cac4043eae704ce9
luffy981 <jhonsmith981@hotmail.com>
2021-11-08 10:51:45 -0500

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,23 @@ import uuid
 from datetime import datetime
 """Air bnb proyect!!!"""
 
+date = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None:
+            for key in kwargs:
+                if key != '__class__':
+                    setattr(self, key, kwargs[key])
+            if hasattr(self, 'created_at') and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], date)
+            if hasattr(self, 'updated_at') and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], date)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
