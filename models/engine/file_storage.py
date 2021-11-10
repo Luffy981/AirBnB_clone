7247d46bc0605686981a2bcfd1db47f7c71bec86
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-11-10 14:14:34 -0500

    importing classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+"""File Storage module"""
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
 
 
 classes = {"BaseModel": BaseModel}
