267ef43a00b4eb082267157e3a19ac222eb82e78
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-11-10 14:26:35 -0500

    add import folder models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """File Storage module"""
 import json
+import models
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
+from models.review import BaseModel
+
 
 
 classes = {"BaseModel": BaseModel}
