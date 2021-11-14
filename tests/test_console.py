02caba8b766811ae6e7206f76a98d92fb834d976
luffy981 <jhonsmith981@hotmail.com>
2021-11-14 11:10:23 -0500

    fixed

--- tests/test_console.py
+++ tests/test_console.py
@@ -366,27 +366,3 @@ class ShowTest(unittest.TestCase):
             HBNBCommand().onecmd("User.update()".format(id))
         self.assertEqual(f.getvalue(), expectect)
 
-    def test_count(self):
-        """Validate count method"""
-        try:
-            os.remove("file.json")
-        except Exception as f:
-            pass
-
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create User")
-            HBNBCommand().onecmd("create BaseModel")
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("User.count()")
-        self.assertNotEqual(f.getvalue(), '')
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("id.count()")
-            expectect = "*** Unknown syntax: id.count()\n"
-        self.assertEqual(f.getvalue(), expectect)
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("User.count(d)")
-        self.assertEqual(f.getvalue(), '*** Unknown syntax: User.count(d)\n')
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("User.count()d")
-        self.assertEqual(f.getvalue(), '*** Unknown syntax: User.count()d\n')
-
