daf85d008afcdebb289ce7bb3f50ee47e4c4369e
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-11-14 21:56:48 -0500

    fix file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,7 +6,8 @@ Unit tests for Base class
 
 import unittest
 from models import storage
-import pep8
+# import pep8
+import pycodestyle
 from models.base_model import BaseModel
 
 
@@ -20,9 +21,9 @@ class Test_Base(unittest.TestCase):
         new_object = storage.all()
         self.assertEqual(dict, type(new_object))
 
-    def test_pep8_conformance(self):
+    def test_pep8(self):
         """Test PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
+        pep8style = pycodestyle.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
