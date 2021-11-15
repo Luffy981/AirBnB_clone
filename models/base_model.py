b5ab10a656d84e135adf021cb082b63069903e12
luffy981 <jhonsmith981@hotmail.com>
2021-11-14 23:08:01 -0500

    base model fixed

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,7 @@ date = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel"""
+        self.id = str(uuid.uuid4())
         if kwargs is not None and kwargs != {}:
             for key in kwargs.keys():
                 if key != '__class__':
@@ -21,7 +22,6 @@ class BaseModel:
             if hasattr(self, 'updated_at') and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], date)
         else:
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
